bebadaed5c29152c182ee3230b938460612dc019
MrMaxWhite <353789930@qq.com>
2021-07-13 17:00:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 #!/usr/bin/env node
+
+// 使用node开发命令行工具所执行的js脚本必须在顶部写#!/usr/bin/env node
+
+// 用于处理用户输入的命令，可以自动解析命令和参数
 const program = require('commander')
+// 下载并提取git仓库，如下载上传到git中的模板
 const download = require('download-git-repo')
+// 模板引擎，将用户提交的信息动态填充到文件中
 const handlebars = require('handlebars')
+// 通用的命令行用户界面集合，用于和用户进行交互
 const inquirer = require('inquirer')
 const fs = require('fs')
+// 在终端显示下载动画
 const ora = require('ora')
+// 可以给终端的字体格式化，如加颜色、改变字体等
 const chalk = require('chalk')
+// 可以在终端中显示出叉叉或打钩符号
 const logSymbols = require('log-symbols')
 const templates = require('./templates')
 
