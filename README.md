80b127c0b4281311cddbe618528853d10ba04462
captain <353789930@qq.com>
2023-09-09 20:08:18 +0800

    补充提示

--- README.md
+++ README.md
@@ -9,3 +9,28 @@
 3.经过模板引擎渲染定制的项目模板
 
 4.当模板有变动时，只需要更新github上的模板即可，无需用户更新脚手架
+
+## 如何生成命令，如在终端执行w-cli
+1、npm init -y  生成package.json
+2、在package.json配置 
+  "bin": {
+    "w-cli": "index.js"
+  }
+3、连接到全局，在当前目录执行 npm link，然后在终端就可以使用命令w-cli
+4、接触用命令  npm unlink
+
+## 命令行工具参数设计
+w-cli -h|--help 查看使用帮助
+w-cli -V|--version 查看工具的版本
+w-cli list 列出所有可用模板
+w-cli init <template-name> <project-name> 基于指定的模板进行项目初始化，<>表示必填
+
+## npm发包
+1、打开npmjs.com官方
+2、注册一个npm账号
+3、在npm检索是否有重名的包名
+4、将 package.json 中的 name 修改为发布到npm上的包名(和本地项目名称无关)
+5、打开控制台，执行 npm login，在控制台登录 npm
+6、登录成功后，在项目下执行 npm publish，发布成功后npm官网上要等一段时间才能查询到
+7、本地测试，执行npm i w-cli -g (如果本地原来开发的时候就npm link过呀先执行npm unlink)
+8、执行w-cli的相关命令
--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 #!/usr/bin/env node
 
+// 告诉执行器使用node来执行当前脚本代码
 // 使用node开发命令行工具所执行的js脚本必须在顶部写#!/usr/bin/env node
 
 // 用于处理用户输入的命令，可以自动解析命令和参数
 const program = require('commander')
-// 模板引擎，将用户提交的信息动态填充到文件中
+// 模板引擎，将用户提交的信息动态填充到package.json文件中
 const handlebars = require('handlebars')
-// 通用的命令行用户界面集合，用于和用户进行交互
+// [命令行交互]通用的命令行用户界面集合，用于和用户进行交互
 const inquirer = require('inquirer')
 const fs = require('fs')
-// 在终端显示下载动画
+// [视觉美化]在终端显示下载动画
 const ora = require('ora')
-// 可以给终端的字体格式化，如加颜色、改变字体等
+// [视觉美化]可以给终端的字体格式化，如加颜色、改变字体等
 const chalk = require('chalk')
-// 可以在终端中显示出叉叉或打钩符号
+// [视觉美化]可以在终端中显示出叉叉或打钩符号
 const logSymbols = require('log-symbols')
+
 const templates = require('./templates')
+
+// process.argv 原生获取命令行的方式，可获取到用户输入的命令，获取到的是一个数组，
+// 如在终端输入 w-cli init a b c , init所在数组的位置是2，a在3，以此类推
+// 适用原生获取比较麻烦，可借助commander模块处理
 
 program
   .version('1.0.0') // -v 或--version 输出版本号
@@ -26,19 +32,19 @@ program
   .action((templateName, projectName) => {
     console.log(templateName, projectName)
     // loading 提示
-    const spinner = ora('Downloading the template...').start()
+    const spinner = ora('Downloading the template...').start() //正在下载中
 
     // git clone模板
     let exec = require('child_process').exec;
     let cmd = 'git clone https://github.com/MrMaxWhite/w-cli-template-1';
     exec(cmd,{ encoding: 'utf-8' },function(error, stdout, stderr) {
       if (error) {
-        spinner.fail()
+        spinner.fail() //下载失败
         console.log(logSymbols.error, chalk.red(error))
         return
       }
 
-      spinner.succeed()
+      spinner.succeed() //下载成功
 
       // 重命名文件
       fs.renameSync(`./w-cli-${templateName}`,`./${projectName}`)
